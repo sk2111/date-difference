d00edd770fb1e40cdf93b88ad45717019f160c42
sk2111 <sathisht1996@gmail.com>
2021-05-08 14:33:55 +0530

    Add : DOM manipulation
    Signed-off-by: sk2111 <sathisht1996@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+//Helper to create DOM node
+const createDOMNode = (tagName, content = '', attribute = []) => {
+    const elem = document.createElement(tagName);
+    elem.innerText = content;
+    if (Array.isArray(attribute) && attribute.length) {
+        attribute.forEach(({ name, value }) => {
+            elem.setAttribute(name, value);
+        });
+    }
+    return elem;
+};
 
+// creating DOM nodes
+const dateDiv = createDOMNode('div', '', [{ name: 'class', value: 'container' }]);
+const inputDate = createDOMNode('input', '', [{ name: 'id', value: 'date-input' }, { name: 'type', value: 'date' }]);
+const button = createDOMNode('button', 'DisplayData', [{ name: 'class', value: 'display-btn' }, { name: 'onclick', value: 'displayData()' }]);
+const displayNode = createDOMNode('div', '', [{ name: 'id', value: 'display-content' }]);
+
+//Attach to DOM node
+dateDiv.append(inputDate, button);
+document.body.append(dateDiv, displayNode);
 const dateRef = document.getElementById("date-input");
 const displayRef = document.getElementById("display-content");
 
-
+//calculate month difference between given dates
 const monthDifference = (currentDate, userDate, diffYear) => {
     const monthDiff = currentDate.getMonth() - userDate.getMonth();
     return monthDiff + (12 * diffYear);
@@ -27,14 +47,13 @@ const displayData = () => {
                 <p>Given input date is ${userDate}</p>
                 <p>Year ${diffYear}</p>
                 <p>Month ${monthDifference(currentDate, userDate, diffYear)}</p>
-                <p>Day ${parseInt(diffDay)}</p>
-                <p>Hour ${parseInt(diffHour)}</p>
-                <p>Minutes ${parseInt(diffMin)}</p>
-                <p>Seconds ${parseInt(diffSeconds)}</p>
-                <p>Milli Seconds ${parseInt(diffMilliSeconds)}</p>
+                <p>Day ${Math.floor(diffDay)}</p>
+                <p>Hour ${Math.floor(diffHour)}</p>
+                <p>Hour ${Math.floor(diffHour)}</p>
+                <p>Minutes ${Math.floor(diffMin)}</p>
+                <p>Seconds ${Math.floor(diffSeconds)}</p>
+                <p>Milli Seconds ${Math.floor(diffMilliSeconds)}</p>
             `;
-
-
             displayRef.innerHTML = template;
         }
     }
